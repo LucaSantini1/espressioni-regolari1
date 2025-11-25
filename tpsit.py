ec5a800b3e33ec02c33bd1ff784a204e074c1e4a
LucaSantini1 <luca.santini@studenti.isissgobetti.it>
2025-11-25 15:29:32 +0100

    x

--- tpsit.py
+++ tpsit.py
@@ -6,8 +6,7 @@ class Espreg :
         return re.fullmatch(self.espressione_regolare, stringa) is not None
     
 def main():
-    espressione = input("Inserisci un'espressione regolare: ")
-    espreg = Espreg(espressione)
+    espreg = Espreg(r"(0[1-9]|1[0-9]|2[0-9]|3[0-1])/(0[1-9]|1[0-2])\/(19[0-9][0-9]|20[0-1][0-9]|20[2][0-5])")
     stringa = input("Inserisci una stringa da confrontare: ")
     if espreg.match(stringa):
         print("La stringa corrisponde all'espressione regolare.")
